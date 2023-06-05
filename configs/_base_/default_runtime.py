87308c5a80dbc00289f26677a2c783e4534cba5d
pyzbuaa <pyzbuaa@gmail.com>
2023-06-05 15:46:09 +0800

    add tensorboard and wandb to config

--- configs/_base_/default_runtime.py
+++ configs/_base_/default_runtime.py
@@ -14,7 +14,7 @@ env_cfg = dict(
     dist_cfg=dict(backend='nccl'),
 )
 
-vis_backends = [dict(type='LocalVisBackend')]
+vis_backends = [dict(type='LocalVisBackend'), dict(type='TensorboardVisBackend'), dict(type='WandbVisBackend')]
 visualizer = dict(
     type='mmdet.DetLocalVisualizer',
     vis_backends=vis_backends,
